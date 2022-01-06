b10e24d9b6f906e58964031ab347b72009b99af0
Lilyl03 <66005163+Lilyl03@users.noreply.github.com>
2022-01-06 11:08:00 +0400

    Update hw2.java

--- src/main/java/hw2.java
+++ src/main/java/hw2.java
@@ -148,4 +148,38 @@ public class hw2 {
 	    }
 	    return "They are the same";
 	}
+	//#7
+	public static String funcdaysInMonth(int month, int year){
+	    switch(month){
+	        case 1:
+	            return "January" + year + " has 31 days";
+	        case 2:
+	            if (year % 400 == 0 ||( year % 100 != 0 && year % 4 == 0)){
+	               return "Feburary " + year + " has 29 days";
+	            }
+	            return "Feburary " + year + " has 28 days";
+	        case 3:
+	            return "March " + year + " has 31 days";
+	        case 4:
+	            return "April " + year + " has 30 days";
+	        case 5:
+	            return "May " + year + " has 31 days";
+	        case 6:
+	            return "June " + year + " has 30 days";
+	        case 7:
+	            return "July " + year + " has 31 days";
+	        case 8:
+	            return "August " + year + " has 31 days";
+	        case 9:
+	            return "September " + year + " has 30 days";
+	       case 10:
+	            return "October " + year + " has 31 days";
+	       case 11:
+	            return "November " + year + " has 30 days";
+            case 12:
+	            return "December " + year + " has 31 days";
+            default: 
+                return "This month does not exist";
+	    }
+	}
 }
