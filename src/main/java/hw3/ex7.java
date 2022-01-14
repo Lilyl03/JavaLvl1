a65fd6daf466ab7e330fe51180b6e010d3ca2732
Lilyl03 <66005163+Lilyl03@users.noreply.github.com>
2022-01-14 19:54:32 +0400

    Update ex7.java

--- src/main/java/hw3/ex7.java
+++ src/main/java/hw3/ex7.java
@@ -1,4 +1,21 @@
 package hw3;
 
 public class ex7 {
+  	public static void main(String[] args) {
+	  Scanner input = new Scanner(System.in);
+	  System.out.println("Input the term for Fibonacci sequence:");
+	  int n = input.nextInt();
+	  int n0 = 0;
+	  int n1 = 1;
+	  int nk = n0 +n1;
+	  int cnt = nk + n1;
+	  System.out.println("The sum of the "+ n + " term sequence of Fibonacci is: ");
+	  for(int i = 0 ; i< n-3; i++){
+	      n0 = n1;
+	      n1 = nk;
+	      nk = n0 + n1;
+	      cnt +=nk;
+	  }
+	  System.out.println(cnt);
+	}
 }
