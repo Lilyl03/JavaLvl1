6fdd9b897d489a1355a532857f4036555cb24f13
lluser <lilyllilyl03@gmail.com>
2022-01-23 15:01:46 +0400

    The "UserCreation" solution

---
+++ src/main/java/com/aca/homework/week2/user/creation/User.java
@@ -0,0 +1,6 @@
+package com.aca.homework.week2.user.creation;
+
+public class User {
+    int height;
+    String name;
+}
---
+++ src/main/java/com/aca/homework/week2/user/creation/UserCreation.java
@@ -0,0 +1,26 @@
+package com.aca.homework.week2.user.creation;
+
+public class UserCreation {
+    public static void main(String[] args) {
+        User[] users = new User[3];
+        User user = inputUser("Mary", 170);
+        users[0] = user;
+        user = inputUser("Grace", 169);
+        users[1] = user;
+        user = inputUser("Nicolos",175);
+        users[2] = user;
+        printUser(users[0]);
+        printUser(users[1]);
+        printUser(users[2]);
+    }
+    public static User inputUser(String name,int height){
+        User user = new User();
+        user.name = name;
+        user.height = height;
+        return user;
+    }
+    public static void printUser(User user){
+        System.out.println("The name is: " + user.name +" " + user.name + "'s, height is: " + user.height);
+    }
+
+}
