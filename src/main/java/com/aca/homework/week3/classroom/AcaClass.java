4502de224d1e855f756b74cb33090d5804e4b87f
Lilyl03 <lilyllilyl03@gmail.com>
2022-01-31 13:58:06 +0400

    The solution of 'Classroom'

--- src/main/java/com/aca/homework/week3/classroom/AcaClass.java
+++ src/main/java/com/aca/homework/week3/classroom/AcaClass.java
@@ -9,13 +9,17 @@ public class AcaClass {
 
     public static void main(String[] args) {
         AcaClass acaClass = new AcaClass();
+        Student student1 = new Student();
+        student1.name = "Lisa";
+        student1.surname = "La";
+        Student student2 = new Student();
+        student2.name = "Susan";
+        student2.surname = "Santiago";
+        Student[] student = {student1,student2};
         acaClass.startMillis = 1_640_980_800;
         acaClass.endMillis = 1_651_348_800;
         acaClass.name =  "java level 1";
         acaClass.fee = 500000;
-        acaClass.students[0].name = "Lisa";
-        acaClass.students[0].surname = "La";
-        acaClass.students[1].name = "Susan";
-        acaClass.students[1].surname = "Santiago";
+        acaClass.students = student;
     }
 }
