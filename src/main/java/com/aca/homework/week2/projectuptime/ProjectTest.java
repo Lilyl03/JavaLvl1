1b709852c23b5b05b8c5cc855b2e14cbb1114c06
Lilyl03 <lilyllilyl03@gmail.com>
2022-01-31 15:50:24 +0400

    The solution of 'Project'

--- src/main/java/com/aca/homework/week2/projectuptime/ProjectTest.java
+++ src/main/java/com/aca/homework/week2/projectuptime/ProjectTest.java
@@ -5,22 +5,21 @@ import java.util.Scanner;
 public class ProjectTest {
     public static void main(String[] args) {
         Project project = new Project();
-        Project[] projects = new Project[3];
-        createProject(projects);
+        Project[] projects = createProject();
         printProjects(projects);
     }
-    public static Project createProject( Project[] projects){
-        Project project = new Project();
+    public static Project[] createProject(){
         long startTime = System.currentTimeMillis();
-        for(int i =0; i<3;i++){
-        project = new Project();
+        Project[] projects = new Project[3];
+        for(int i =0; i<3; i++){
+        Project project = new Project();
         System.out.println("Please enter the project name:");
         project.name = new Scanner(System.in).nextLine();
         long current = System.currentTimeMillis();
         project.creationSeconds = (current - startTime)/1000;
         projects[i] = project;
         }
-        return project;
+        return projects;
 
     }
     public static void printProjects(Project[] projects){
