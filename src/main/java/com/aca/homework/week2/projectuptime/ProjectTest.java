6d32b3cceea21e6320a2efe78bcf22c76740ad88
Lilyl03 <lilyllilyl03@gmail.com>
2022-02-02 21:41:56 +0400

    The solution of 'ProjectUpTime'

--- src/main/java/com/aca/homework/week2/projectuptime/ProjectTest.java
+++ src/main/java/com/aca/homework/week2/projectuptime/ProjectTest.java
@@ -4,26 +4,27 @@ import java.util.Scanner;
 
 public class ProjectTest {
     public static void main(String[] args) {
-        Project project = new Project();
         Project[] projects = createProject();
         printProjects(projects);
     }
-    public static Project[] createProject(){
+
+    public static Project[] createProject() {
         long startTime = System.currentTimeMillis();
         Project[] projects = new Project[3];
-        for(int i =0; i<3; i++){
-        Project project = new Project();
-        System.out.println("Please enter the project name:");
-        project.name = new Scanner(System.in).nextLine();
-        long current = System.currentTimeMillis();
-        project.creationSeconds = (current - startTime)/1000;
-        projects[i] = project;
+        for (int i = 0; i < 3; i++) {
+            Project project = new Project();
+            System.out.println("Please enter the project name:");
+            project.name = new Scanner(System.in).nextLine();
+            long current = System.currentTimeMillis();
+            project.creationSeconds = (current - startTime) / 1000;
+            projects[i] = project;
         }
         return projects;
 
     }
-    public static void printProjects(Project[] projects){
-        for(int i =0;i<3;i++){
+
+    public static void printProjects(Project[] projects) {
+        for (int i = 0; i < 3; i++) {
             System.out.println(projects[i].creationSeconds + " " + projects[i].name);
         }
     }
