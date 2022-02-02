ed3454800b1ac4ae705d26c7a2be510b66cceb3d
Lilyl03 <lilyllilyl03@gmail.com>
2022-02-02 15:59:50 +0400

    The solution of 'Exam'

--- src/main/java/com/aca/homework/week2/cars/CarsTest.java
+++ src/main/java/com/aca/homework/week2/cars/CarsTest.java
@@ -3,33 +3,35 @@ package com.aca.homework.week2.cars;
 public class CarsTest {
     public static void main(String[] args) {
         Car[] car = new Car[41];
-        Car cars = new Car();
-        cars.brand = "Porshe";
-        int k = 0;
         for(int i = 1980;i<=2020; i++){
-            Car cars1 = new Car();
-            cars1.year = i;
-            car[k] = cars1;
-            k++;
+            Car cars = new Car();
+            cars.brand = "Porshe";
+            cars.year = i;
+            car[i-1980] = cars;
         }
         Car[] carsAfter2010 = carsAfter2010(car);
-        printCars(carsAfter2010, cars);
+        printCars(carsAfter2010);
     }
 
-    private static void printCars(Car[] car, Car cars) {
+    private static void printCars(Car[] car ) {
         for(int i = 0; i < car.length;i++){
-            System.out.println(cars.brand + " " + car[i].year);
+            System.out.println(car[i].brand + " " + car[i].year);
         }
     }
 
     private static Car[] carsAfter2010(Car[] car) {
-        Car[] c = new Car[11];
-        int k = 0;
-        for(int i = 30; i<=40;i++){
+        int k =0;
+        for(int i = 0; i<car.length;i++){
+            if(car[i].year>=2010){
+                k++;
+            }
+        }
+        Car[] c = new Car[k];
+        for(int i =car.length-k; i< car.length;i++){
             Car car1 = new Car();
+            car1.brand = car[i].brand;
             car1.year = car[i].year;
-            c[k] = car1;
-            k++;
+            c[car.length-i-1] = car1;
         }
         return c;
     }
