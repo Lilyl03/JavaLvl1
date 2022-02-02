96707987d3806808bf3e02e262451dd0627acdf7
Lilyl03 <lilyllilyl03@gmail.com>
2022-02-02 21:52:36 +0400

    The solution of 'numbers: max  and min'

--- src/main/java/com/aca/homework/week3/numbers/max/min/Numbers.java
+++ src/main/java/com/aca/homework/week3/numbers/max/min/Numbers.java
@@ -5,28 +5,24 @@ public class Numbers {
     private long number2;
     private long number3;
 
-public Numbers(int num1,int num2, int num3){
-    this.number1 = num1;
-    this.number2 = num2;
-    this.number3 = num3;
-}
-public long max(){
-    if(this.number1 >= this.number2 && this.number1 >= this.number3){
-        return this.number1;
-    }else if(this.number2 >= this.number1 && this.number2 >= this.number3){
-        return this.number2;
-    }else{
-        return this.number3;
+    public Numbers(int num1, int num2, int num3) {
+        this.number1 = num1;
+        this.number2 = num2;
+        this.number3 = num3;
+
     }
-}
-    public long min(){
-        if(this.number1 <= this.number2 && this.number1 <= this.number3){
-            return this.number1;
-        }else if(this.number2 <= this.number1 && this.number2 <= this.number3){
-            return this.number2;
-        }else{
-            return this.number3;
-        }
+
+    public void getMaxAndMin() {
+        System.out.println("The max is : " + max(number1, max(number2, number3)));
+        System.out.println("The min is : " + min(number1, max(number2, number3)));
+    }
+
+    public long max(long num1, long num2) {
+        return num1 > num2 ? num1 : num2;
+    }
+
+    public long min(long num1, long num2) {
+        return num1 < num2 ? num1 : num2;
     }
 
 }
--- src/main/java/com/aca/homework/week3/numbers/max/min/NumbersTest.java
+++ src/main/java/com/aca/homework/week3/numbers/max/min/NumbersTest.java
@@ -3,9 +3,6 @@ package com.aca.homework.week3.numbers.max.min;
 public class NumbersTest {
     public static void main(String[] args) {
         Numbers numbers = new Numbers(78,98,154);
-        System.out.println("The max is:");
-        System.out.println(numbers.max());
-        System.out.println("The min is:");
-        System.out.println(numbers.min());
+        numbers.getMaxAndMin();
     }
 }
