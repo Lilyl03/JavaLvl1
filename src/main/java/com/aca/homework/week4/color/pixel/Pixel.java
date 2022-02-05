659e7370cdaa85cd1489e4e4df8571569acd3bd4
Lilyl03 <lilyllilyl03@gmail.com>
2022-02-06 02:42:29 +0400

    The solution of the color pixel problem

---
+++ src/main/java/com/aca/homework/week4/color/pixel/Pixel.java
@@ -0,0 +1,27 @@
+package com.aca.homework.week4.color.pixel;
+
+public class Pixel {
+    private int x;
+    private int y;
+
+    public Pixel(int x, int y) {
+        setX(x);
+        setY(y);
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
