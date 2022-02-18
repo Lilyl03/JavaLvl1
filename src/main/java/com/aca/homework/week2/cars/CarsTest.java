cbf30cfa4da915f22825d3e61bd86801ecb49369
Lilyl03 <lilyllilyl03@gmail.com>
2022-02-18 22:04:58 +0400

    The solution of 'Cars'

--- src/main/java/com/aca/homework/week2/cars/CarsTest.java
+++ src/main/java/com/aca/homework/week2/cars/CarsTest.java
@@ -2,35 +2,41 @@ package com.aca.homework.week2.cars;
 
 public class CarsTest {
     public static void main(String[] args) {
-        Car[] car = new Car[41];
-        for(int i = 1980;i<=2020; i++){
-            Car cars = new Car();
-            cars.brand = "Porsche";
-            cars.year = i;
-            car[i-1980] = cars;
+        Car[] cars = new Car[41];
+        printCars(carsAfter2010(addCars(cars)));
+    }
+
+    private static Car[] addCars(Car[] cars) {
+        for (int i = 1980; i <= 2020; i++) {
+            Car car = new Car();
+            car.brand = "Porsche";
+            car.year = i;
+            cars[i - 1980] = car;
         }
-        Car[] carsAfter2010 = carsAfter2010(car);
-        printCars(carsAfter2010);
+        return cars;
     }
 
     private static void printCars(Car[] cars) {
-        for(int i = 0; i < cars.length;i++){
+        for (int i = 0; i < cars.length; i++) {
             System.out.println(cars[i].brand + " " + cars[i].year);
         }
     }
 
-    private static Car[] carsAfter2010(Car[] car) {
-        int k =0;
-        for(int i = 0; i<car.length;i++){
-            if(car[i].year>=2010){
-                k++;
+    private static Car[] carsAfter2010(Car[] cars) {
+        int cnt = 0;
+        for (int i = 0; i < cars.length; i++) {
+            if (cars[i].year >= 2010) {
+                cnt++;
             }
         }
-        Car[] c = new Car[k];
-        for(int i =car.length-k; i< car.length;i++){
-            c[car.length-i-1] = car[i];
+        Car[] carsAfter2010 = new Car[cnt];
+        for (int i = 0; i < cars.length; i++) {
+            if (cars[i].year >= 2010) {
+                carsAfter2010[--cnt] = cars[i];
+            }
+
         }
-        return c;
+        return carsAfter2010;
     }
 
 }
