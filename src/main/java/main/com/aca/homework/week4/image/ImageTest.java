34a93f8b0ca9a83d99f8c72328164200d7070514
Lilyl03 <lilyllilyl03@gmail.com>
2022-03-06 19:15:21 +0400

    hw

--- src/main/java/main/com/aca/homework/week4/image/ImageTest.java
+++ src/main/java/main/com/aca/homework/week4/image/ImageTest.java
@@ -1,13 +1,5 @@
-package main.com.aca.homework.homework.week4.image;
+package main.com.aca.homework.week4.image;
 
 public class ImageTest {
-    public static void main(String[] args) {
-        Image image = new Image(5,10);
-        image.show();
-        image.draw(1,2);
-        image.draw(2,1);
-        image.draw(2,2);
-        image.draw(2,3);
-        image.show();
-    }
+
 }
