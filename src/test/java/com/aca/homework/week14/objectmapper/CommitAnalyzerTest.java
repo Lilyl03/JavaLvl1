859e9d51ebed36de0e771feba12dcc566862991e
Lilyl03 <lilyllilyl03@gmail.com>
2022-04-15 20:50:36 +0400

    The solution of the 'SQL students' problem

---
+++ src/test/java/com/aca/homework/week14/objectmapper/CommitAnalyzerTest.java
@@ -0,0 +1,31 @@
+package com.aca.homework.week14.objectmapper;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+
+
+import java.io.IOException;
+import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommitAnalyzerTest {
+    public CommitAnalyzer testSubject;
+    @BeforeEach
+    public void init(){
+        testSubject = new CommitAnalyzer(new Supplier<String>() {
+            @Override
+            public String get() {
+                return "/home/luser/IdeaProjects/JavaLvl1/src/main/java/com/aca/homework/week14/objectmapper/commits-data.json";
+            }
+        });
+    }
+
+
+    @Test
+    public void testCount() throws IOException {
+        Assertions.assertEquals(301,testSubject.count());
+    }
+
+}
