933af1af5722f64022219bbebb006688f0b7d50d
Lilyl03 <lilyllilyl03@gmail.com>
2022-04-19 22:16:23 +0400

    update

---
+++ src/main/java/com/aca/homework/week9/generic/queue/QueueEmptyException.java
@@ -0,0 +1,7 @@
+package com.aca.homework.week9.generic.queue;
+
+public class QueueEmptyException extends RuntimeException {
+    public QueueEmptyException() {
+        super("The Queue is empty");
+    }
+}
